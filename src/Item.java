08f03073843ee8627debd46c3b20fe90b6715391
Toadsta <78148898+Toadsta@users.noreply.github.com>
2022-04-07 21:55:34 +0100

    Add- added some initial attributes

---
+++ src/Item.java
@@ -0,0 +1,9 @@
+import java.math.BigDecimal;
+
+public class Item {
+    private String itemName;
+    private BigDecimal itemValue;
+    private String itemCategory;
+
+    //note to self. maybe contain allergies?
+}
